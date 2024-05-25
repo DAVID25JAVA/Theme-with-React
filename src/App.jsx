30d1c348f2e7a7817606faedc87030997f48f3b8
David_pal <dp2072000@gmail.com>
2024-05-26 03:34:22 +0530

    change in code

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import {   useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import ThemeBtn from "./components/ThemeBtn";
 import Card from "./components/Card";
 import { ThemeContextProvider } from "./context/ThemeContext";
 
 function App() {
-  const [themeMode, setThemeMode] = useState('light');
+  const [themeMode, setThemeMode] = useState("light");
 
   const lightTheme = () => {
     setThemeMode("light");
@@ -15,26 +15,24 @@ function App() {
   };
 
   useEffect(() => {
-    document.querySelector('html').classList.remove("light", "dark");
-    document.querySelector('html').classList.add(themeMode);
+    document.querySelector("html").classList.remove("light", "dark");
+    document.querySelector("html").classList.add(themeMode);
   }, [themeMode]);
 
   return (
-    < ThemeContextProvider value={{lightTheme, darkTheme }}>
+    <ThemeContextProvider value={{ lightTheme, darkTheme }}>
       <div className="flex flex-wrap min-h-screen items-center">
         <div className="w-full">
           <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
-            
             <ThemeBtn />
           </div>
-          
+
           <div className="w-full max-w-sm mx-auto">
-            
             <Card />
           </div>
         </div>
       </div>
-    </ ThemeContextProvider>
+    </ThemeContextProvider>
   );
 }
 
