fc93fce178b988a6a6e68a6838c7a18f74770e0d
David_pal <dp2072000@gmail.com>
2024-05-26 03:30:16 +0530

    some change in code

--- src/App.jsx
+++ src/App.jsx
@@ -24,12 +24,12 @@ function App() {
       <div className="flex flex-wrap min-h-screen items-center">
         <div className="w-full">
           <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
-            {/* <ThemeBtn /> */}
+            
             <ThemeBtn />
           </div>
           
           <div className="w-full max-w-sm mx-auto">
-            {/* <Card /> */}
+            
             <Card />
           </div>
         </div>
